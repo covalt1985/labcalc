76be0f7f12a193b77f39d082bdcc668ea0a41063
Tomasz Kowalik <tomaszkowalik85@gmail.com>
2021-04-23 00:04:32 +0200

    add logic to components

--- src/App.js
+++ src/App.js
@@ -4,10 +4,43 @@ import './App.css';
 import Menu from './Components/Menu/index';
 
 class App extends Component {
+  static defaultProps = {
+    tests: [
+      'HOMA IR',
+      'Wapń Skorygowany',
+      'Wapń / g Kreatyniny',
+      'Odzysk Prolaktyny',
+    ],
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: '',
+      isClicked: false,
+    };
+    this.renderMenuItem = this.renderMenuItem.bind(this);
+  }
+
+  renderMenuItem(item) {
+    this.setState({ item: [item], isClicked: true });
+  }
+
   render() {
+    const isClicked = this.state.isClicked;
     return (
       <div>
-        <Menu />
+        {!isClicked && (
+          <Menu
+            menuItems={[...this.props.tests]}
+            clickedItem={this.renderMenuItem}
+          />
+        )}
+        {isClicked && (
+          <Menu
+            menuItems={[...this.state.item]}
+            clickedItem={this.renderMenuItem}
+          />
+        )}
       </div>
     );
   }
--- src/Components/Menu/index.js
+++ src/Components/Menu/index.js
@@ -3,14 +3,26 @@ import React, { Component } from 'react';
 import './style.css';
 
 class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.props.clickedItem(e.target.innerText);
+  }
+
   render() {
     return (
-      <div class="wrapper">
-        <ul class="menu">
-          <li>HOMA IR</li>
-          <li>Wapń Skorygowany</li>
-          <li>Wapń / g Kreatyniny</li>
-          <li>Odzysk Prolaktyny</li>
+      <div className="wrapper">
+        <ul className="menu">
+          {this.props.menuItems.map(item => {
+            return (
+              <li key={item} onClick={this.handleClick}>
+                {item}
+              </li>
+            );
+          })}
         </ul>
       </div>
     );
