3b8ef700c7c9e40ba96b1afa450fa3e0caf003b1
Tomasz Kowalik <tomaszkowalik85@gmail.com>
2021-04-22 23:25:06 +0200

    initial commit with menu

---
+++ src/Components/Menu/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import './style.css';
+
+class Menu extends Component {
+  render() {
+    return (
+      <div class="wrapper">
+        <ul class="menu">
+          <li>HOMA IR</li>
+          <li>Wapń Skorygowany</li>
+          <li>Wapń / g Kreatyniny</li>
+          <li>Odzysk Prolaktyny</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Menu;
